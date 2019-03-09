ab6d41971bd59dff3b55f5d36e1b74623d77d3f2
Duvan Reyes <duvan.reyessaavedra@gmail.com>
2019-03-09 15:00:38 -0500

    carpeta reportes gestion humana

--- src/java/Controlador/ReportesContolador.java
+++ src/java/Controlador/ReportesContolador.java
@@ -19,19 +19,23 @@ import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.LinkedList;
-import java.util.List;
 import javax.ejb.EJB;
 import java.sql.DriverManager;
 import java.util.HashMap;
 import java.util.Map;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.util.JRLoader;
 
 
 /**
@@ -108,30 +112,34 @@ public String datosGraficaUno() throws SQLException{
     
     
     
-   public void exportar(int idinsumo, String nombreinsumo) throws ClassNotFoundException, SQLException, JRException, IOException {
-        try {
-            FacesContext fc = FacesContext.getCurrentInstance();
-            ExternalContext ec = fc.getExternalContext();
-            File jasper = new File(ec.getRealPath("../Reportes/report1.jasper"));
-            Map<String, Object> params = new HashMap<>();
-            params.put("pIn", idinsumo);
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_carrito", "root", "");
-            JasperPrint jp = JasperFillManager.fillReport(jasper.getPath(), params, conexion);
-            HttpServletResponse hsr = (HttpServletResponse) ec.getResponse();
-            hsr.addHeader("Content-disposition", "attachment; filename=Procutos"+nombreinsumo+".pdf");
-            OutputStream os = hsr.getOutputStream();
-            JasperExportManager.exportReportToPdfStream(jp, os);
-            os.flush();
-            os.close();
-            fc.responseComplete();
-        } catch (JRException ex) {
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-
-        }
-
+   public void exportarContrato(int idempleado) throws ClassNotFoundException, SQLException, JRException, IOException {
+       Map<String, Object> parametros = new HashMap<>();
+       parametros.put("idempleado", idempleado);
+       //conexiones
+       Class.forName("com.mysql.jdbc.Driver");
+       Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/MEDIUSSOFTWARE", "root", "");
+       //Para buscar el reporte
+        File buscareporte = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("Paginas/ReportesGestionHumana/Contrato.jasper"));
+       //Para llenar el reporte
+       JasperPrint llenareporte=JasperFillManager.fillReport(buscareporte.getPath(),parametros ,conexion);
+       //permitir que el browser habra el archivo
+       //Visibilidad del reporte
+        HttpServletResponse visualizareporte = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+        
+       //nombre cabecera del reporte
+        visualizareporte.addHeader("Content-disposition", "attachment; filename=Contrato.pdf");
+        ServletOutputStream stream =visualizareporte.getOutputStream();
+        
+        //Generar el pdf tipo de formato
+        JasperExportManager.exportReportToPdfStream(llenareporte, stream);
+        
+        //Cierro el response
+        stream.flush();
+        stream.close();
+        FacesContext.getCurrentInstance().responseComplete();
     }
+   
+  
+
     
 }
